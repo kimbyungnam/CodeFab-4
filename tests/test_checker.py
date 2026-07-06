6a10770b88bfcb8b80dc115a4ca744e2989fcd6e
junwon <junwle2@gmail.com>
2026-07-06 16:36:44 +0900

    refactor: 선언 전 사용 에러 검출 테스트 Mocker 사용

--- tests/test_checker.py
+++ tests/test_checker.py
@@ -1,26 +1,29 @@
 import pytest
 
-from codefab.ast_nodes import Binary, ExpressionStmt, Variable
 from codefab.checker import Checker
-from codefab.tokens import Token, TokenType
+from codefab.token import Token, TokenType
 
 
 def test_정상_입력_확인():
     pass
 
 
-def test_선언_전_사용_에러_검출():
+def test_선언_전_사용_에러_검출(mocker):
     # arrange
-    exp_statement = ExpressionStmt(
-        Binary(
-            left=Variable(
-                Token(type=TokenType.IDENTIFIER, lexeme="a", literal=None, line=1)
-            ),
-            operator=Token(type=TokenType.PLUS, lexeme="+", literal=None, line=1),
-            right=Variable(
-                Token(type=TokenType.IDENTIFIER, lexeme="b", literal=None, line=1)
-            ),
-        )
+    variable_a = mocker.Mock()
+    variable_a.name = Token(type=TokenType.IDENTIFIER, lexeme="a", literal=None, line=1)
+    variable_a.accept.side_effect = lambda visitor: visitor.visit_variable(variable_a)
+
+    variable_b = mocker.Mock()
+    variable_b.name = Token(type=TokenType.IDENTIFIER, lexeme="b", literal=None, line=1)
+    variable_b.accept.side_effect = lambda visitor: visitor.visit_variable(variable_b)
+
+    binary_expr = mocker.Mock(left=variable_a, right=variable_b)
+    binary_expr.accept.side_effect = lambda visitor: visitor.visit_binary(binary_expr)
+
+    exp_statement = mocker.Mock(expression=binary_expr)
+    exp_statement.accept.side_effect = lambda visitor: visitor.visit_expression_stmt(
+        exp_statement
     )
 
     ast = [exp_statement]
