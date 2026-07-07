f10ad2545b0003444568fbf368133350bc2273b3
seungju.cho <sgmath12@gmail.com>
2026-07-07 10:33:25 +0900

    feat: statement 52page

---
+++ tests/test_statement.py
@@ -0,0 +1,172 @@
+from codefab.assembler.expr import Assign, Binary, Literal, Variable
+from codefab.assembler.statement_parser import StatementParser
+from codefab.ast_nodes import (
+    BlockStmt,
+    ExpressionStmt,
+    ForStmt,
+    IfStmt,
+    PrintStmt,
+    VarStmt,
+)
+from codefab.tokens import Token, TokenType
+
+
+def test_if_stmt_without_else_is_parsed_as_if_stmt():
+    # if (x > 0) y = 1;
+    tokens = [
+        Token(TokenType.IF, "if", literal=None, line=1),
+        Token(TokenType.LEFT_PAREN, "(", literal=None, line=1),
+        Token(TokenType.IDENTIFIER, "x", literal=None, line=1),
+        Token(TokenType.GREATER, ">", literal=None, line=1),
+        Token(TokenType.NUMBER, "0", literal=0.0, line=1),
+        Token(TokenType.RIGHT_PAREN, ")", literal=None, line=1),
+        Token(TokenType.IDENTIFIER, "y", literal=None, line=2),
+        Token(TokenType.EQUAL, "=", literal=None, line=2),
+        Token(TokenType.NUMBER, "1", literal=1.0, line=2),
+        Token(TokenType.SEMICOLON, ";", literal=None, line=2),
+        Token(TokenType.EOF, "", literal=None, line=2),
+    ]
+
+    statements = StatementParser(tokens).parse()
+
+    assert len(statements) == 1
+    stmt = statements[0]
+    assert isinstance(stmt, IfStmt)
+
+    assert isinstance(stmt.condition, Binary)
+    assert stmt.condition.operator.lexeme == ">"
+    assert isinstance(stmt.condition.left, Variable)
+    assert stmt.condition.left.name.lexeme == "x"
+    assert stmt.condition.right == Literal(0.0)
+
+    assert isinstance(stmt.then_branch, ExpressionStmt)
+    assert isinstance(stmt.then_branch.expression, Assign)
+    assert stmt.then_branch.expression.name.lexeme == "y"
+    assert stmt.then_branch.expression.value == Literal(1.0)
+
+    assert stmt.else_branch is None
+
+
+def test_if_stmt_with_else_is_parsed_as_if_stmt():
+    # if (x > 0) y = 1; else y = 2;
+    tokens = [
+        Token(TokenType.IF, "if", literal=None, line=1),
+        Token(TokenType.LEFT_PAREN, "(", literal=None, line=1),
+        Token(TokenType.IDENTIFIER, "x", literal=None, line=1),
+        Token(TokenType.GREATER, ">", literal=None, line=1),
+        Token(TokenType.NUMBER, "0", literal=0.0, line=1),
+        Token(TokenType.RIGHT_PAREN, ")", literal=None, line=1),
+        Token(TokenType.IDENTIFIER, "y", literal=None, line=1),
+        Token(TokenType.EQUAL, "=", literal=None, line=1),
+        Token(TokenType.NUMBER, "1", literal=1.0, line=1),
+        Token(TokenType.SEMICOLON, ";", literal=None, line=1),
+        Token(TokenType.ELSE, "else", literal=None, line=1),
+        Token(TokenType.IDENTIFIER, "y", literal=None, line=1),
+        Token(TokenType.EQUAL, "=", literal=None, line=1),
+        Token(TokenType.NUMBER, "2", literal=2.0, line=1),
+        Token(TokenType.SEMICOLON, ";", literal=None, line=1),
+        Token(TokenType.EOF, "", literal=None, line=1),
+    ]
+
+    stmt = StatementParser(tokens).parse()[0]
+
+    assert isinstance(stmt, IfStmt)
+    assert isinstance(stmt.then_branch, ExpressionStmt)
+    assert isinstance(stmt.else_branch, ExpressionStmt)
+    assert stmt.else_branch.expression.value == Literal(2.0)
+
+
+def test_block_stmt_groups_multiple_statements():
+    # { x = 1; y = 2; }
+    tokens = [
+        Token(TokenType.LEFT_BRACE, "{", literal=None, line=1),
+        Token(TokenType.IDENTIFIER, "x", literal=None, line=1),
+        Token(TokenType.EQUAL, "=", literal=None, line=1),
+        Token(TokenType.NUMBER, "1", literal=1.0, line=1),
+        Token(TokenType.SEMICOLON, ";", literal=None, line=1),
+        Token(TokenType.IDENTIFIER, "y", literal=None, line=1),
+        Token(TokenType.EQUAL, "=", literal=None, line=1),
+        Token(TokenType.NUMBER, "2", literal=2.0, line=1),
+        Token(TokenType.SEMICOLON, ";", literal=None, line=1),
+        Token(TokenType.RIGHT_BRACE, "}", literal=None, line=1),
+        Token(TokenType.EOF, "", literal=None, line=1),
+    ]
+
+    stmt = StatementParser(tokens).parse()[0]
+
+    assert isinstance(stmt, BlockStmt)
+    assert len(stmt.statements) == 2
+    assert all(isinstance(s, ExpressionStmt) for s in stmt.statements)
+
+
+def test_print_stmt_is_parsed_as_print_stmt():
+    # print a;
+    tokens = [
+        Token(TokenType.PRINT, "print", literal=None, line=1),
+        Token(TokenType.IDENTIFIER, "a", literal=None, line=1),
+        Token(TokenType.SEMICOLON, ";", literal=None, line=1),
+        Token(TokenType.EOF, "", literal=None, line=1),
+    ]
+
+    stmt = StatementParser(tokens).parse()[0]
+
+    assert isinstance(stmt, PrintStmt)
+    assert isinstance(stmt.expression, Variable)
+    assert stmt.expression.name.lexeme == "a"
+
+
+def test_for_stmt_is_parsed_as_for_stmt():
+    # for (var i = 0; i < 3; i = i + 1) { print i; }
+    tokens = [
+        Token(TokenType.FOR, "for", literal=None, line=1),
+        Token(TokenType.LEFT_PAREN, "(", literal=None, line=1),
+        Token(TokenType.VAR, "var", literal=None, line=1),
+        Token(TokenType.IDENTIFIER, "i", literal=None, line=1),
+        Token(TokenType.EQUAL, "=", literal=None, line=1),
+        Token(TokenType.NUMBER, "0", literal=0.0, line=1),
+        Token(TokenType.SEMICOLON, ";", literal=None, line=1),
+        Token(TokenType.IDENTIFIER, "i", literal=None, line=1),
+        Token(TokenType.LESS, "<", literal=None, line=1),
+        Token(TokenType.NUMBER, "3", literal=3.0, line=1),
+        Token(TokenType.SEMICOLON, ";", literal=None, line=1),
+        Token(TokenType.IDENTIFIER, "i", literal=None, line=1),
+        Token(TokenType.EQUAL, "=", literal=None, line=1),
+        Token(TokenType.IDENTIFIER, "i", literal=None, line=1),
+        Token(TokenType.PLUS, "+", literal=None, line=1),
+        Token(TokenType.NUMBER, "1", literal=1.0, line=1),
+        Token(TokenType.RIGHT_PAREN, ")", literal=None, line=1),
+        Token(TokenType.LEFT_BRACE, "{", literal=None, line=1),
+        Token(TokenType.PRINT, "print", literal=None, line=1),
+        Token(TokenType.IDENTIFIER, "i", literal=None, line=1),
+        Token(TokenType.SEMICOLON, ";", literal=None, line=1),
+        Token(TokenType.RIGHT_BRACE, "}", literal=None, line=1),
+        Token(TokenType.EOF, "", literal=None, line=1),
+    ]
+
+    stmt = StatementParser(tokens).parse()[0]
+
+    assert isinstance(stmt, ForStmt)
+    assert isinstance(stmt.initializer, VarStmt)
+    assert isinstance(stmt.condition, Binary)
+    assert isinstance(stmt.increment, Assign)
+    assert isinstance(stmt.body, BlockStmt)
+
+
+def test_var_declaration_with_initializer_is_parsed_as_var_stmt():
+    # var x = 10;
+    tokens = [
+        Token(TokenType.VAR, "var", literal=None, line=1),
+        Token(TokenType.IDENTIFIER, "x", literal=None, line=1),
+        Token(TokenType.EQUAL, "=", literal=None, line=1),
+        Token(TokenType.NUMBER, "10", literal=10.0, line=1),
+        Token(TokenType.SEMICOLON, ";", literal=None, line=1),
+        Token(TokenType.EOF, "", literal=None, line=1),
+    ]
+
+    statements = StatementParser(tokens).parse()
+
+    assert len(statements) == 1
+    stmt = statements[0]
+    assert isinstance(stmt, VarStmt)
+    assert stmt.name.lexeme == "x"
+    assert stmt.initializer == Literal(10.0)
