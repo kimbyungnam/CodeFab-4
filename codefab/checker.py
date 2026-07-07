e6717170a181d434a14418292ad6bf48a79cc974
junwon <junwle2@gmail.com>
2026-07-07 15:55:33 +0900

    feat: 테스트 공백 보완 및 통과 코드 구현

--- codefab/checker.py
+++ codefab/checker.py
@@ -1,4 +1,19 @@
-from codefab.ast_nodes import Binary, BlockStmt, ExpressionStmt, Stmt, Variable, VarStmt
+from codefab.ast_nodes import (
+    Assign,
+    Binary,
+    BlockStmt,
+    ExpressionStmt,
+    ForStmt,
+    Grouping,
+    IfStmt,
+    Literal,
+    Logical,
+    PrintStmt,
+    Stmt,
+    Unary,
+    Variable,
+    VarStmt,
+)
 from codefab.error import (
     DuplicateVariableError,
     SelfReferenceInInitializerError,
@@ -46,3 +61,37 @@ class Checker:
     def visit_binary(self, expr: Binary):
         expr.left.accept(self)
         expr.right.accept(self)
+
+    def visit_literal(self, expr: Literal):
+        pass
+
+    def visit_grouping(self, expr: Grouping):
+        expr.expression.accept(self)
+
+    def visit_unary(self, expr: Unary):
+        expr.right.accept(self)
+
+    def visit_logical(self, expr: Logical):
+        expr.left.accept(self)
+        expr.right.accept(self)
+
+    def visit_assign(self, expr: Assign):
+        expr.value.accept(self)
+
+    def visit_print_stmt(self, stmt: PrintStmt):
+        stmt.expression.accept(self)
+
+    def visit_if_stmt(self, stmt: IfStmt):
+        stmt.condition.accept(self)
+        stmt.then_branch.accept(self)
+        if stmt.else_branch is not None:
+            stmt.else_branch.accept(self)
+
+    def visit_for_stmt(self, stmt: ForStmt):
+        if stmt.initializer is not None:
+            stmt.initializer.accept(self)
+        if stmt.condition is not None:
+            stmt.condition.accept(self)
+        if stmt.increment is not None:
+            stmt.increment.accept(self)
+        stmt.body.accept(self)
