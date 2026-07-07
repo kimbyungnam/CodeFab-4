50b0172214c9971f4b8504c3ea168e22713a3a16
hyunjun <chlguswns96@gmail.com>
2026-07-07 10:12:55 +0900

    feat: failing case 추가

--- tests/test_assembler.py
+++ tests/test_assembler.py
@@ -1,6 +1,7 @@
 import pytest
 
 from codefab.assembler.assembler import Assembler
+from codefab.assembler.errors import ParseError
 
 
 class _FakeStatementParser:
@@ -13,6 +14,21 @@ class _FakeStatementParser:
         return len(self._statements) == 0
 
     def parse_statement(self):
+        return self._statements.pop(0)
+
+
+class _FailingStatementParser:
+    """일부 statement 성공 후 ParseError 를 던지는 테스트 더블."""
+
+    def __init__(self, statements_then_error):
+        self._statements = list(statements_then_error)
+
+    def is_at_end(self):
+        return False
+
+    def parse_statement(self):
+        if not self._statements:
+            raise ParseError("의도적으로 발생시킨 오류", line=1)
         return self._statements.pop(0)
 
 
@@ -30,3 +46,10 @@ def test_assemble_returns_statements_in_order(statements):
     program = Assembler(statement_parser).assemble()
 
     assert program == statements
+
+
+def test_assemble_propagates_parse_error_without_swallowing():
+    statement_parser = _FailingStatementParser(["stmt-1"])
+
+    with pytest.raises(ParseError):
+        Assembler(statement_parser).assemble()
