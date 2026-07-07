1e3d36c11ef55c9c6d6c9f17d0428c5582734038
junwon <junwle2@gmail.com>
2026-07-07 10:40:45 +0900

    feat: 다른 스코프에서는 동일한 변수명 재선언 허용 테스트/통과 코드 구현 추가

--- codefab/checker.py
+++ codefab/checker.py
@@ -3,8 +3,12 @@ from codefab.ast_nodes import Binary, BlockStmt, ExpressionStmt, Stmt, Variable,
 
 class Checker:
     def __init__(self):
-        self.declared: set[str] = set()
+        self.scopes: list[set[str]] = [set()]
         self.initializing: str | None = None
+
+    @property
+    def declared(self) -> set[str]:
+        return self.scopes[-1]
 
     def resolve(self, statements: list[Stmt]):
         for stmt in statements:
@@ -14,22 +18,24 @@ class Checker:
         stmt.expression.accept(self)
 
     def visit_block_stmt(self, stmt: BlockStmt):
+        self.scopes.append(set())
         for statement in stmt.statements:
             statement.accept(self)
+        self.scopes.pop()
 
     def visit_var_stmt(self, stmt: VarStmt):
-        if stmt.name.lexeme in self.declared:
+        if stmt.name.lexeme in self.scopes[-1]:
             raise ValueError("이미 선언된 변수입니다.")
         if stmt.initializer is not None:
             self.initializing = stmt.name.lexeme
             stmt.initializer.accept(self)
             self.initializing = None
-        self.declared.add(stmt.name.lexeme)
+        self.scopes[-1].add(stmt.name.lexeme)
 
     def visit_variable(self, expr: Variable):
         if expr.name.lexeme == self.initializing:
             raise ValueError("지역 변수 자기 참조 에러입니다.")
-        if expr.name.lexeme not in self.declared:
+        if not any(expr.name.lexeme in scope for scope in self.scopes):
             raise ValueError("선언되지 않은 변수를 사용했습니다.")
 
     def visit_binary(self, expr: Binary):
